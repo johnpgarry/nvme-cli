55b11455954b6535f9e832b60b0b53aa487a1658
John Garry <john.g.garry@oracle.com>
2026-06-03 12:08:25 +0100

    nvme: fail reset on busy controller

--- nvme.c
+++ nvme.c
@@ -1933,6 +1933,7 @@ static int scan_dev_filter(const struct dirent *d)
 			return 0;
 		if (sscanf(d->d_name, "nvme%dn%dp%d", &ctrl, &ns, &part) == 3)
 			return 0;
+		printf("%s10 d->d_name=%s *FOUND*\n", __func__, d->d_name);
 		return 1;
 	}
 	return 0;
@@ -3176,23 +3177,78 @@ ret:
 static int reset(int argc, char **argv, struct command *cmd, struct plugin *plugin)
 {
 	const char *desc = "Resets the NVMe controller\n";
-	int err, fd;
+	struct dirent **devices;
+	int err, fd, n, i;
+	const char *force = "The \"I know what I'm doing\" flag, skip confirmation before sending command";
+
+	printf("%s\n", __func__);
+
+
+	struct config {
+		int force;
+	};
+
+	struct config cfg = {
+		.force        = 0,
+	};
+
 
 	const struct argconfig_commandline_options command_line_options[] = {
+		{"force",        'f', "",     CFG_NONE,     &cfg.force,        no_argument,       force},
 		{NULL}
 	};
 
-	fd = parse_and_open(argc, argv, desc, command_line_options, NULL, 0);
-	if (fd < 0) {
-		err = fd;
+	err = argconfig_parse(argc, argv, desc, command_line_options, &cfg, sizeof(cfg));
+	printf("%s1 after argconfig_parse err=%d\n", __func__, err);
+	if (err)
+		goto ret;
+
+	n = scandir(dev, &devices, scan_dev_filter, alphasort);
+	if (n < 0) {
+		fprintf(stderr, "no NVMe device(s) detected.\n");
+		err = n;
 		goto ret;
 	}
 
-	err = nvme_reset_controller(fd);
+	for (i = 0; i < n; i++) {
+		int myfd;
+		char myname[512];
+		int flags = 0;
+
+		snprintf(myname, sizeof(myname), "%s%s", dev, devices[i]->d_name);
+		if (!cfg.force)
+			flags |= O_EXCL;
+		myfd = open_dev(myname, flags);
+
+		if (myfd >= 0) {
+			close(myfd);
+		} else {
+			if (myfd == EBUSY) {
+				fprintf(stderr, "Failed to open %s.\n",
+					basename(argv[optind]));
+				fprintf(stderr,
+					"Namespace is currently busy.\n"
+					"Use the force [--force|-f] option to ignore that.\n");
+			} else {
+				argconfig_print_help(desc, command_line_options);
+			}
+			goto cleanup_devices;
+		}
+	}
+
+	err = fd = parse_and_open(argc, argv, desc, command_line_options, NULL, 0);
+
+	err = 0;//nvme_reset_controller(fd);
 	if (err < 0)
 		perror("Reset");
 
 	close(fd);
+
+cleanup_devices:
+	for (i = 0; i < n; i++)
+		free(devices[i]);
+	free(devices);
+
 ret:
 	return nvme_status_to_errno(err, false);
 }
